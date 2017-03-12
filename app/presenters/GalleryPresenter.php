e574e41abd12a5d5a6001b99d8abddb6904d938d
WellBloud <peter.holop@gmail.com>
2017-03-12 14:09:54 +0100

    - added favicons - added gallery detail view

--- app/presenters/GalleryPresenter.php
+++ app/presenters/GalleryPresenter.php
@@ -15,24 +15,51 @@ class GalleryPresenter extends BasePresenter
 
     private $albumsFolder;
     private $albumsThumbsFolder;
+    private $album;
     private $albums;
+    private $photos;
 
     public function __construct(AppSettings $appSettings)
     {
         parent::__construct($appSettings);
         $this->albumsFolder = $this->appSettings->getParameter(['images', 'albumsFolder']);
         $this->albumsThumbsFolder = $this->appSettings->getParameter(['images', 'albumsThumbsFolder']);
+        $this->albums = $this->getAlbums();
     }
 
     public function actionDefault()
     {
-        $this->albums = $this->getAlbums();
+
+    }
+
+    public function actionDetail($id)
+    {
+        foreach ($this->albums as $album) {
+            if ($album['webalized'] === $id) {
+                $this->album = $album;
+                break;
+            }
+        }
+        if ($this->album === NULL) {
+            $this->flashMessage('Album nenalezeno', 'warning');
+            $this->redirect('Gallery:');
+        }
+        $photos = $this->getFiles($this->albumsFolder . $this->album['title']);
+        $this->photos = array_diff($photos, [$this->albumsThumbsFolder]);
     }
 
     public function renderDefault()
     {
         $this->template->albums = $this->albums;
         $this->template->albumsFolder = $this->albumsFolder;
+    }
+
+    public function renderDetail()
+    {
+        $this->template->album = $this->album;
+        $this->template->photos = $this->photos;
+        $this->template->albumsFolder = $this->albumsFolder;
+        $this->template->albumsThumbsFolder = $this->albumsThumbsFolder;
     }
 
     private function getAlbums()
@@ -43,11 +70,11 @@ class GalleryPresenter extends BasePresenter
         }
         $final = [];
         foreach ($albums as $album) {
-            $photos = $this->getFiles($this->albumsFolder . $album . $this->albumsThumbsFolder);
+            $photos = $this->getFiles($this->albumsFolder . $album . '/' . $this->albumsThumbsFolder);
             $final[] = [
                 'title' => $album,
                 'webalized' => Strings::webalize($album),
-                'photo' => $this->albumsFolder . $album . $this->albumsThumbsFolder . '/' . $photos[array_rand($photos)]
+                'photo' => $this->albumsFolder . $album . '/' . $this->albumsThumbsFolder . '/' . $photos[array_rand($photos)]
             ];
         }
         return $final;
